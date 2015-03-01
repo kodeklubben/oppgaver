65aba756ad58d3a7e09285210fee59c459e1f67d
kwrl <haakongk@stud.ntnu.no>
2015-03-01 12:44:03 -0500

    deploy.sh no longer fails when there is no README.md present in the destination dir

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,13 @@
 #Set CWD to the location of this file
 cd ${0%/*}
 ./gulp links && \
-cd kodeklubben.github.io && \
-find * ! -name README.md -delete && \
+cd kwrl.github.io && \
+if [ -e README.md ]
+then
+    find * ! -name README.md -delete 
+else
+    rm -rf *
+fi
 cp -r ../build/* . && \
 git add --all . && \
 git commit -m "build $(date)" && \
