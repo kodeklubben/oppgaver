f9a3c2c90bcb8700f30a246f5c654bf6d8cae17d
arve0 <arve.seljebu@gmail.com>
2015-09-14 09:39:44 +0200

    gjør automatisk bygg mer robust

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-#Set CWD to the location of this file
+# Set CWD to the location of this file
 cd ${0%/*}
+git pull && \
+cd codeclub_lesson_builder && \
+git fetch origin nb-NO && \
+git merge --no-edit FETCH_HEAD && \
+npm install && \
+cd .. && \
 ./gulp dist && \
 ./gulp links && \
-cd kodeklubben.github.io && \
+cd dist && \
 if [ -e README.md ]
 then
     find * ! -name README.md -delete
