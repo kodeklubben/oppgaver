5fb4ff07371d4c82e09bbad7f03721b215bd7b9a
vegardbb <vegardbb@stud.ntnu.no>
2016-12-07 01:05:09 +0100

    Legg til løsningsforslag i bondegaard

---
+++ src/python/bondegaard/bondegaard_solution.py
@@ -0,0 +1,34 @@
+innhegning = { "Shaun" : "Lam", "Toralf" : "Kalv", "Oskar" : "Vær", "Stella" : "Søye",
+               "Bobo" : "Lam", "Per" : "Søye", "Baba" : "Vær", "Kjell" : "Okse",
+               "Staur" : "Okse", "Sune" : "Lam", "Rune" : "Lam", "Tune" : "Lam",
+               "Scar" : "Søye", "Petra" : "Søye", "Litago" : "Ku", "Kakao" : "Ku",
+               "Dauingen" : "Gris", "Gris" : "Gris", "Frøya" : "Søye", "Egon" : "Vær",
+               "Kevin" : "Vær", "Tora" : "Søye", "Ralf" : "Kalv", "Enga" : "Ku", "Kjell" : "Vær",
+               "Kjell" : "Gris", "Benedikte" : "Ku", "Agnethe" : "Søye", "Grisskjit" : "Søye",
+               "Bjarne" : "Søye", "Hoppsideisi" : "Søye", "Koskos" : "Lam", "Boms" : "Lam",
+               "Mulle" : "Lam", "Queen of England" : "Søye", "Dronningen av Sverige" : "Søye",
+               "Rosenborg" : "Søye", "Lyn" : "Kalv", "Stråmann" : "Vær", "Blåmann" : "Okse",
+               "Grynte" : "Gris", "Pål" : "Kalv", "Askguri" : "Ku", "Hansen" : "Lam", "Du" : "Søye",
+               "Bente" : "Søye", "Marit" : "Søye", "Martin" : "Lam", "Pelle" : "Lam", "Anita" : "Lam",
+               "Bernt" : "Vær", "Jompa" : "Lam", "Grislam" : "Lam", "Ku" : "Vær", "OK" : "Søye", "ArveLarve" : "Lam",
+               "Sigurd" : "Lam", "Janne" : "Søye", "Nikita" : "Søye", "Saul" : "Søye", "Bonde" : "Søye",
+               "Thor" : "Vær", "FandenIhelsike" : "Okse", "Mathis" : "Kalv", "Kalle" : "Kalv", "VonKarme" : "Søye",
+               "Vegard" : "Okse", "Sindre" : "Lam", "Toffe" : "Søye", "Dennis" : "Søye", "Prebz" : "Vær",
+               "Theodore" : "Ku", "Turbine" : "Søye", "Guri" : "Søye", "Karl" : "Ku", "Karianne" : "Lam", "Grisveitj" : "Søye",
+               "Grismainn" : "Okse", "Gress" : "Søye", "Esben" : "Lam", "Prut" : "Søye", "Kola" : "Søye", "Peter" : "Søye", "Lom" : "Vær" }
+
+def dyretelling(dyr):
+    antall_dyr = 0
+    #SKRIV KODE HER
+    return antall_dyr
+
+def main():
+    dyreliste = [ "Ku", "Okse", "Kalv", "Søye", "Vær", "Lam" ]
+    dyr = input("Skriv inn hvilket dyr som skal telles opp: ")
+    while dyr not in dyreliste:
+        print("Ugyldig dyr.")
+            dyr = input("Skriv inn hvilket dyr som skal telles opp: ")
+        print(dyretelling(dyr))
+
+main()
+
