e7a77c4ac313a27293c2340a9be492b7367f4680
arve0 <arve.seljebu@gmail.com>
2017-06-14 17:09:24 +0200

    fjern phantomjs fra travis setup

--- travis-install.sh
+++ travis-install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 git submodule init && \
 git submodule update --depth 1 && \
-cd codeclub_lesson_builder && \
-sed -i "/^.*\"browser-sync\".*$/d" package.json && \
+cd codeclub_lesson_builder
+
+# do not install browser sync
+sed -i "/^.*\"browser-sync\".*$/d" package.json
+
+# do not install phantomjs
+sed -i "/^.*\"metalsmith-phantomjs-pdf\".*$/d" package.json
+
 npm install
