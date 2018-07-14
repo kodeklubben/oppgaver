e1f275100aeeb56b482f85f14f2726ea4488ae57
Øistein Søvik <Oistein.Sovik.92@gmail.com>
2018-07-14 16:27:39 +0200

    Started writing on a syntax_check

---
+++ syntax_check.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+found=false
+
+#Color and bold
+red_bold='\033[1;31m'
+green_bold='\033[1;32m'
+nc='\033[0m' # No color
+
+# Explanation of options given to grep:
+# -r                                    recursive
+# -i                                    ignore case
+# -q                                    quitet, do not print
+# -P                                    use Perl syntax (not that many backslashes needed)
+# --include=\*.tex                      only search in texfiles (the * is a wildcard)
+# --exclude-dir={Utkommentert,Meta}     do not search in (any) directory called "Utkommentert" or "Meta"
+
+# Finds images without ALT tags, Markdown syntax ![]()
+# if grep -rniP --color=auto --include=\*.md '\!\[\]\(.*\)$' ./src; then
+#     printf "\nFound missing ALT: ${red_bold}\$\$${nc}.\n\n"
+#     found=true
+# fi
+
+# Finds images without ALT tags, HTML syntax ![]()
+# if grep -rniP --color=auto --include=\*.md '<img(?![^>]*\balt=)[^>]*?>' ./src; then
+#     printf "\nFound missing ALT: ${red_bold}\$\$${nc}.\n\n"
+#     found=true
+# fi
+
+# Finds every title with an incorrect title, it should be # WORD {.WORD}.
+if (grep -r --include=\*.md '^##* ' ./src | grep -v '{.*}$'); then
+    printf "\nIncorrect title, should be # WORD {.TYPE}: ${red_bold}\$\$${nc}.\n\n"
+    found=true
+fi
+
+
+# Exit with error status (1) if there was a match
+if [ "${found}" = true ]; then
+    exit 1
+fi
